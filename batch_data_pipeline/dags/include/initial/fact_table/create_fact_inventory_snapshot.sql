acd4f5f5494d4c3e17476c9328a6ebd8c06a3389
ruonanRuby <rniu3@wisc.edu>
2021-07-07 19:53:43 +0800

    finished the second part and screenshot all the result BI graph stored in docs >> metabase results >> part 1 / part 2

--- batch_data_pipeline/dags/include/initial/fact_table/create_fact_inventory_snapshot.sql
+++ batch_data_pipeline/dags/include/initial/fact_table/create_fact_inventory_snapshot.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS fact_inventory_snapshot(
     product_id VARCHAR NOT NULL,
     amount DECIMAL,
-    date_id    VARCHAR,
+    date_id    date,
     processed_time TIMESTAMP,
     UNIQUE(product_id)
 );
